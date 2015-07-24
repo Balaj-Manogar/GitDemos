94e3263e28641a592d40543d49baa61945098d5f
Balaji <rajanbalaji2000@yahoo.co.in>
2015-07-24 17:50:09 +0530

    Bootstrap tests

--- js/app.js
+++ js/app.js
@@ -2,3 +2,24 @@
 
 var a = 10;
 var b = 20;
+
+$(document).ready(function()
+{
+  $('.testC').carousel();
+  $('.testC .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+});
