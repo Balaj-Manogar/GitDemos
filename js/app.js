9ac0e8c288b5ca2b729f87e2c7850586a61a98c4
Balaji <rajanbalaji2000@yahoo.co.in>
2015-07-25 16:45:49 +0530

    carousel with content

--- js/app.js
+++ js/app.js
@@ -12,9 +12,9 @@ $('.testC .item').each(function(){
   if (!next.length) {
     next = $(this).siblings(':first');
   }
-  //next.children(':first-child').clone().appendTo($(this));
+  next.children(':first-child').clone().appendTo($(this));
 
-  for (var i=0;i<2;i++) {
+  for (var i=0;i<1;i++) {
     next=next.next();
     if (!next.length) {
     	next = $(this).siblings(':first');
