e42ba76d9b163db283792726cb6009b857702eed
Balaji M <rajanbalaji2000@yahoo.co.in>
2015-07-26 09:33:15 +0530

    responsive carousel using jcarouse

--- js/app.js
+++ js/app.js
@@ -5,7 +5,52 @@ var b = 20;
 
 $(document).ready(function() {
 
-  $('.testC').carousel();
+  $('#trendingJcarousel')
+    .on('jcarousel:create jcarousel:reload', function() {
+      var element = $(this),
+        width = element.innerWidth();
+
+      if (width > 900) {
+        width = width / 3;
+      } else if (width > 600) {
+        width = width / 2;
+      } else if (width < 600) {
+        width = width;
+      }
+
+      element.jcarousel('items').css('width', width + 'px');
+    })
+    .jcarousel({
+      wrap: 'circular'
+    });
+
+  $('.jcarousel-control-prev')
+    .jcarouselControl({
+      target: '-=2'
+    });
+
+  $('.jcarousel-control-next')
+    .jcarouselControl({
+      target: '+=2'
+    });
+
+  $('.jcarousel-pagination')
+    .on('jcarouselpagination:active', 'a', function() {
+      $(this).addClass('active');
+    })
+    .on('jcarouselpagination:inactive', 'a', function() {
+      $(this).removeClass('active');
+    })
+    .on('click', function(e) {
+      e.preventDefault();
+    })
+    .jcarouselPagination({
+      perPage: 2,
+      item: function(page) {
+        return '<a href="#' + page + '">' + page + '</a>';
+      }
+    });
+  /*$('#trendingCarousel').carousel();
   $('.testC .item').each(function() {
     var next = $(this).next();
     if (!next.length) {
@@ -24,4 +69,5 @@ $(document).ready(function() {
       next = next.next();
     }
   });
+  */
 });
